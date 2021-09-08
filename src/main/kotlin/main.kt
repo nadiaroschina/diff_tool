74de63aef1e8e8bc2a5350c467491594acfcc62e
nadiaroschina <nadysha.rr@gmail.com>
2021-09-08 21:00:53 +0300

    project template: function headings, file input and output

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,31 @@
-fun main(args: Array<String>) {
+import java.io.File
+import java.io.PrintWriter
+
+
+// LCS function finds the largest common sequence of strings in two given texts
+fun LCS(text1: List<String>, text2: List<String>) : List<String> {
     TODO()
 }
+
+// diff function creates the edit script for two given text using LCS function
+fun diff(originalText: List<String>, newText: List<String>) : List<String> {
+    TODO()
+}
+
+
+fun main(args: Array<String>) {
+
+    // input data: files originalText.txt and newText.txt
+    val originalText = File("src/main/kotlin/originalText.txt").readLines()
+    val newText = File("src/main/kotlin/newText.txt").readLines()
+
+    // using diff function
+    val diffText = diff(originalText, newText)
+
+    // output data: file resText.txt
+    val resText = PrintWriter("src/main/kotlin/resText.txt")
+    for (line in diffText) resText.println(line)
+    resText.close()
+
+}
+
