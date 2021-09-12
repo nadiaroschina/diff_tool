52977ebdc1cf6b8f6db37f413290ddd635a39d4d
nadiaroschina <nadysha.rr@gmail.com>
2021-09-12 13:35:38 +0300

    LCS function realization

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,50 @@
 import java.io.File
 import java.io.PrintWriter
+import kotlin.math.max
 
 
 // LCS function finds the largest common sequence of strings in two given texts
 fun LCS(text1: List<String>, text2: List<String>) : List<String> {
-    TODO()
+
+    // res[i][j] stores the length of LCS for the first i strings of text1 and first j strings of text2
+    val res : Array<Array<Int>> = Array(text1.size + 1) { Array(text2.size + 1) { 0 } }
+
+    // dynamic programming realization
+    for (i in text1.indices) {
+        for (j in text2.indices) {
+            if (text1[i] == text2[j]) {
+                res[i + 1][j + 1] = res[i][j] + 1
+            }
+            else {
+                res[i + 1][j + 1] = max(res[i][j + 1], res[i + 1][j])
+            }
+        }
+    }
+
+
+    val resText : ArrayList<String> = arrayListOf()
+    var i = text1.size
+    var j = text2.size
+
+    // retracting LCS using res table
+    while (i > 0 && j > 0) {
+        if (res[i - 1][j - 1] < res[i][j]) {
+            resText.add(text1[i - 1])
+            i -= 1
+            j -= 1
+        }
+        else {
+            if (res[i - 1][j] >= res[i][j - 1]) i -= 1
+            else j -= 1
+        }
+    }
+    resText.reverse()
+
+    return resText.toList()
 }
 
 // diff function creates the edit script for two given text using LCS function
-fun diff(originalText: List<String>, newText: List<String>) : List<String> {
+fun diff(originalText: List<String>, newText: List<String>): List<String> {
     TODO()
 }
 
