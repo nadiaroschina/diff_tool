b07414caeffc4fbccd7b4d1fc58b6b2c47dc82ad
nadiaroschina <nadysha.rr@gmail.com>
2021-09-12 14:07:12 +0300

    LCS function corrected

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,8 +3,22 @@ import java.io.PrintWriter
 import kotlin.math.max
 
 
+
+fun printTable(m: Array<Array<Int>>) {
+    print("     ")
+    for (i in m[0].indices) print("$i ")
+    println()
+
+    for (i in m.indices) {
+        print("$i :  ")
+        for (j in m[i].indices) print(m[i][j].toString() + " ")
+        println()
+    }
+}
+
+
 // LCS function finds the largest common sequence of strings in two given texts
-fun LCS(text1: List<String>, text2: List<String>) : List<String> {
+fun lcs(text1: List<String>, text2: List<String>) : List<String> {
 
     // res[i][j] stores the length of LCS for the first i strings of text1 and first j strings of text2
     val res : Array<Array<Int>> = Array(text1.size + 1) { Array(text2.size + 1) { 0 } }
@@ -21,6 +35,7 @@ fun LCS(text1: List<String>, text2: List<String>) : List<String> {
         }
     }
 
+    // printTable(res)
 
     val resText : ArrayList<String> = arrayListOf()
     var i = text1.size
@@ -28,14 +43,16 @@ fun LCS(text1: List<String>, text2: List<String>) : List<String> {
 
     // retracting LCS using res table
     while (i > 0 && j > 0) {
-        if (res[i - 1][j - 1] < res[i][j]) {
-            resText.add(text1[i - 1])
+        if (res[i - 1][j] == res[i][j]) {
             i -= 1
+        }
+        else if (res [i][j - 1] == res[i][j]) {
             j -= 1
         }
         else {
-            if (res[i - 1][j] >= res[i][j - 1]) i -= 1
-            else j -= 1
+            resText.add(text1[i - 1])
+            i -= 1
+            j -= 1
         }
     }
     resText.reverse()
@@ -43,9 +60,10 @@ fun LCS(text1: List<String>, text2: List<String>) : List<String> {
     return resText.toList()
 }
 
+
 // diff function creates the edit script for two given text using LCS function
 fun diff(originalText: List<String>, newText: List<String>): List<String> {
-    TODO()
+    return lcs(originalText, newText)
 }
 
 
