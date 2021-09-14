edc49329e4b6bf3aaa2bddd3615a6c9647f20dc3
nadiaroschina <nadysha.rr@gmail.com>
2021-09-14 10:58:37 +0300

    tests for diff function added

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -1,5 +1,6 @@
 import kotlin.test.*
 
+
 internal class TestLCS {
 
     @Test
@@ -40,5 +41,49 @@ internal class TestLCS {
         assertEquals(List(500) { "1" },
             lcs(List(1000) {(it % 2 + 1).toString()},
                 List(1000) {(it % 2).toString()}))
+    }
+}
+
+
+internal class TestDiff {
+
+    @Test
+    fun smallTestDiff() {
+        // checking correctness
+        assertEquals(listOf(Line('+', "e"), Line('-', "h"), Line('+', "i"), Line('-', "q"), Line('+', "k"), Line('+', "r"), Line('+', "x"), Line('+', "y")),
+            diff(listOf("a", "b", "c", "d", "f", "g", "h", "j", "q", "z"),
+                listOf("a", "b", "c", "d", "e", "f", "g", "i", "j", "k", "r", "x", "y", "z")))
+        assertEquals(listOf(Line('-', "X"), Line('+', "Z"), Line('-', "Y"), Line('+', "W"), Line('+', "X"), Line('-', "Z")),
+            diff(listOf("X", "M", "J", "Y", "A", "U", "Z"),
+                listOf("M", "Z", "J", "A", "W", "X", "U")))
+        assertEquals(listOf(Line('-', "b"), Line('-', "d")),
+            diff(listOf("a", "b", "c", "d", "e"),
+                listOf("a", "c", "e")))
+        assertEquals(listOf(Line('-', "b"), Line('-', "e"), Line('+', "b")),
+            diff(listOf("a", "b", "c", "d", "e"),
+                listOf("a", "c", "d", "b")))
+        assertEquals(listOf(Line('+', "x"), Line('-', "b"), Line('-', "c"), Line('+', "y"), Line('-', "e"), Line('+', "z")),
+            diff(listOf("a", "b", "c", "d", "e"),
+                listOf("x", "a", "y", "d", "z")))
+        assertEquals(listOf(Line('+', "x"), Line('+', "y"), Line('-', "d"), Line('-', "e")),
+            diff(listOf("a", "b", "c", "d", "e"),
+                listOf("x", "y", "a", "b", "c")))
+        assertEquals(listOf(Line('-', "a"), Line('-', "b"), Line('+', "x"), Line('+', "y")),
+            diff(listOf("a", "b", "c", "d", "e"),
+                listOf("c", "d", "e", "x", "y")))
+    }
+
+    @Test
+    fun bigTestDiff() {
+        // checking efficiency
+        assertEquals(emptyList(),
+            diff(List(1000) { "1" },
+                List(1000) { "1" }))
+        assertEquals(List(500) { Line('+', "1") },
+            diff(List(500) { "0" },
+                List(1000) {(it % 2).toString()}))
+        assertEquals(List(500) { Line ('-', "1") },
+            diff(List(1000) {(it % 2).toString()},
+                List(500) { "0" }))
     }
 }
