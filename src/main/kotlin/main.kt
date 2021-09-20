e1e80b62e83ababa9e2f80a27aa10a952769c91f
nadiaroschina <nadysha.rr@gmail.com>
2021-09-20 21:33:36 +0300

    code refactoring: enum class Status used instead of Char to mark added and deleted strings

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -4,9 +4,19 @@ import kotlin.math.max
 import kotlin.system.exitProcess
 
 
-data class Line (val symbol : Char, val string : String) {
+enum class Status {
+    Deleted, Added;
+
     override fun toString(): String {
-        return ("$symbol $string")
+        return if (this == Deleted) "-"
+        else if (this == Added) "+"
+        else ""
+    }
+}
+
+data class Line(val status: Status, val string: String) {
+    override fun toString(): String {
+        return ("$status $string")
     }
 }
 
@@ -16,9 +26,8 @@ fun printTable(m: Array<Array<Int>>) {
     print("     ")
     for (i in m[0].indices) print("$i ")
     println()
-
     for (i in m.indices) {
-        print("$i :  ")
+        print("$i : ")
         for (j in m[i].indices) print(m[i][j].toString() + " ")
         println()
     }
@@ -26,18 +35,17 @@ fun printTable(m: Array<Array<Int>>) {
 
 
 // LCS function finds the largest common sequence of strings in two given texts
-fun lcs(text1: List<String>, text2: List<String>) : List<String> {
+fun lcs(text1: List<String>, text2: List<String>): List<String> {
 
     // res[i][j] stores the length of LCS for the first i strings of text1 and first j strings of text2
-    val res : Array<Array<Int>> = Array(text1.size + 1) { Array(text2.size + 1) { 0 } }
+    val res: Array<Array<Int>> = Array(text1.size + 1) { Array(text2.size + 1) { 0 } }
 
     // dynamic programming realization
     for (i in text1.indices) {
         for (j in text2.indices) {
             if (text1[i] == text2[j]) {
                 res[i + 1][j + 1] = res[i][j] + 1
-            }
-            else {
+            } else {
                 res[i + 1][j + 1] = max(res[i][j + 1], res[i + 1][j])
             }
         }
@@ -45,7 +53,7 @@ fun lcs(text1: List<String>, text2: List<String>) : List<String> {
 
     // printTable(res)
 
-    val resText : ArrayList<String> = arrayListOf()
+    val resText: ArrayList<String> = arrayListOf()
     var i = text1.size
     var j = text2.size
 
@@ -53,11 +61,9 @@ fun lcs(text1: List<String>, text2: List<String>) : List<String> {
     while (i > 0 && j > 0) {
         if (res[i - 1][j] == res[i][j]) {
             i -= 1
-        }
-        else if (res [i][j - 1] == res[i][j]) {
+        } else if (res[i][j - 1] == res[i][j]) {
             j -= 1
-        }
-        else {
+        } else {
             resText.add(text1[i - 1])
             i -= 1
             j -= 1
@@ -72,7 +78,7 @@ fun lcs(text1: List<String>, text2: List<String>) : List<String> {
 // diff function creates the edit script for two given text using LCS function
 fun diff(originalText: List<String>, newText: List<String>): List<Line> {
     val mutualText = lcs(originalText, newText)
-    val resLines : ArrayList<Line> = arrayListOf()
+    val resLines: ArrayList<Line> = arrayListOf()
     // ind1 - current position in original Text, ind2 -  in newText, ind - in mutualText
     var ind1 = 0
     var ind2 = 0
@@ -81,26 +87,24 @@ fun diff(originalText: List<String>, newText: List<String>): List<Line> {
     while (ind1 < originalText.size || ind2 < newText.size) {
         if (ind < mutualText.size) {
             if (originalText[ind1] == newText[ind2] && originalText[ind1] == mutualText[ind]) {
-                // resLines.add(Line(' ', mutualText[ind]))
+                // resLines.add(Line(NotChanged, mutualText[ind]))
                 ind1++
                 ind2++
                 ind++
-            }
-            else if (originalText[ind1] == mutualText[ind]) {
-                resLines.add(Line('+', newText[ind2]))
+            } else if (originalText[ind1] == mutualText[ind]) {
+                resLines.add(Line(Status.Added, newText[ind2]))
                 ind2++
             } else {
-                resLines.add(Line('-', originalText[ind1]))
+                resLines.add(Line(Status.Deleted, originalText[ind1]))
                 ind1++
             }
-        }
-        else {
+        } else {
             while (ind1 < originalText.size) {
-                resLines.add(Line('-', originalText[ind1]))
+                resLines.add(Line(Status.Deleted, originalText[ind1]))
                 ind1++
             }
             while (ind2 < newText.size) {
-                resLines.add(Line('+', newText[ind2]))
+                resLines.add(Line(Status.Added, newText[ind2]))
                 ind2++
             }
         }
@@ -109,7 +113,7 @@ fun diff(originalText: List<String>, newText: List<String>): List<Line> {
 }
 
 
-fun printLines(fileName : String, diffText : List<Line>) {
+fun printLines(fileName: String, diffText: List<Line>) {
     val resText = PrintWriter(fileName)
     for (line in diffText) resText.println(line)
     resText.close()
@@ -118,8 +122,8 @@ fun printLines(fileName : String, diffText : List<Line>) {
 
 fun main(args: Array<String>) {
 
-    // reading input data: file paths
-    // default files would be originalText.txt and newText.txt
+    // reading input data: command line or file paths
+    // for file input default files would be originalText.txt and newText.txt
 
     println("Enter original file path. Press enter to use default file name")
     val input1 = readLine()
@@ -131,7 +135,7 @@ fun main(args: Array<String>) {
 
     val originalText = try {
         File(path1).readLines()
-    } catch (e : Exception) {
+    } catch (e: Exception) {
         println("Invalid file name")
         exitProcess(1)
     }
@@ -147,7 +151,7 @@ fun main(args: Array<String>) {
 
     val newText = try {
         File(path2).readLines()
-    } catch (e : Exception) {
+    } catch (e: Exception) {
         println("Invalid file name")
         exitProcess(1)
     }
